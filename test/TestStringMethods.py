758f839c503965684c578cf96192f40014944421
jarecase <jarecase@hotmail.com>
2022-05-30 12:16:41 -0500

    Update TestStringMethods.py
    23

--- test/TestStringMethods.py
+++ test/TestStringMethods.py
@@ -1,20 +1,24 @@
 import unittest
+import requests
+
+API = 'http://127.0.0.1:5000/api/juego'
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_Get(self):
+        res = requests.get(API)
+        prueba = not str(res.text)
+        self.assertFalse(prueba)
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+    def test_POST(self):
+        res = requests.get(API)
+        prueba = not str(res.text)
+        self.assertFalse(prueba)
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_PUT(self):
+        res = requests.get(API)
+        prueba = not str(res.text)
+        self.assertFalse(prueba)
 
 if __name__ == '__main__':
     unittest.main()
