cb10cc3016ed2e7af55107bd796bac09ac9ae01b
jarecase <jarecase@hotmail.com>
2022-06-13 10:23:29 -0500

    uso de flask

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Reinaldojcs\Desktop\Juarqsoft-API\env
+set VIRTUAL_ENV=C:\Users\Reinaldojcs\Documents\GitHub\Juarqsoft\env
 
 if not defined PROMPT set PROMPT=$P$G
 
