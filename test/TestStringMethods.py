45628621d36ff9619495a2bb967725df95e9c185
jarecase <jarecase@hotmail.com>
2022-06-13 15:55:25 -0500

    Update TestStringMethods.py

--- test/TestStringMethods.py
+++ test/TestStringMethods.py
@@ -1,14 +1,15 @@
 import unittest
 import requests
+from app import app
 
+local = app.test_client().get('/')
 API = 'http://127.0.0.1:5000/api/juego'
 
 class TestStringMethods(unittest.TestCase):
 
     def test_Get(self):
-        res = requests.get(API)
-        prueba = not str(res.text)
-        self.assertFalse(prueba)
+        assert local.status_code == 200
+        assert local.data.decode('utf-8') == 'hello'
 
     def test_POST(self):
         res = requests.get(API)
